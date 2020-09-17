e980920f6bda7135147f05a425b88cdc5461a677
Daniel Soares <>
2020-09-17 20:48:33 -0300

    criei a struct Country

---
+++ SuperHotel/SuperHotel/Country.swift
@@ -0,0 +1,15 @@
+//
+//  Country.swift
+//  SuperHotel
+//
+//  Created by Daniel Soares on 17/09/20.
+//  Copyright © 2020 Rafael Miguel. All rights reserved.
+//
+
+import Foundation
+
+struct Country {
+    
+    var nameCountry: String?
+    
+}
