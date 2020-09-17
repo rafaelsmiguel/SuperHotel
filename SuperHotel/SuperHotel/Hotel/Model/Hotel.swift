8e5a84cf0d6aa90b118e68080c0ed4169ab17322
rafaelsmiguel <rafael.souza.miguel@gmail.com>
2020-09-17 20:43:10 -0300

    adicionando pasta hotel

---
+++ SuperHotel/SuperHotel/Hotel/Model/Hotel.swift
@@ -0,0 +1,17 @@
+//
+//  Hotel.swift
+//  SuperHotel
+//
+//  Created by Rafael Miguel on 17/09/20.
+//  Copyright © 2020 Rafael Miguel. All rights reserved.
+//
+
+import Foundation
+
+struct Hotel {
+    var id: Int?
+    var name: String?
+    var description: String?
+    var lat: Double?
+    var long: Double?
+}
