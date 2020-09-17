d6918e5d04282c8c7a61e81cf32e5608a94a367d
Elder Alcantara <dealcantara@gmail.com>
2020-09-17 20:18:25 -0300

    Inclusão da Struct "Booking"

---
+++ SuperHotel/SuperHotel/Booking.swift
@@ -0,0 +1,17 @@
+//
+//  Booking.swift
+//  SuperHotel
+//
+//  Created by Elder Alcantara on 17/09/20.
+//  Copyright © 2020 Rafael Miguel. All rights reserved.
+//
+
+import Foundation
+
+struct Booking {
+    
+    var title: String?
+    var description: String?
+    
+    
+}
